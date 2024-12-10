8cbb525baa451b906c7f481bd7ba29e8e305f5a3
Zaqoon <zixzone@gmail.com>
2024-12-10 18:40:13 +0100

    Rename export_decks.py to export.py

---
+++ export.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+from dotenv import load_dotenv
+
+load_dotenv()
+USER = os.getenv('USER')
+
+
+def export_decks(path: str):
+    path = f'{path}/gen3_decks/function'
+    origin_path = 'decks/function'
+    folder_list = os.listdir(origin_path)
+    for folder in folder_list:
+        destination = f'{path}/{folder}'
+        if os.path.exists(destination):
+            shutil.rmtree(destination)
+        # Move folder
+        source = f'{origin_path}/{folder}'
+        copy_paste_folder(source, path)
+
+
+def export_boosters(path: str):
+    destination = f'{path}/gen3_boosters/function'
+    source = 'boosters/function'
+    # Delete existing folder
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    # Paste boosters
+    copy_paste_folder(source, f'{path}/gen3_boosters/')
+
+
+def export_expansion_files(path: str):
+    path = f'{path}/expansions/function'
+    source = 'expansions/function'
+    for file in os.listdir(source):
+        source_file = os.path.join(source, file)
+        destination = os.path.join(path, file)
+
+        copy_paste_file(source_file, destination)
+
+
+def copy_paste_file(source: str, destination: str):
+    if os.path.isfile(source):
+        shutil.copy2(source, destination)
+    else:
+        print(f'{source} is not a file.')
+
+
+def copy_paste_folder(source: str, destination: str):
+    try:
+        shutil.copytree(source, destination)
+    except FileExistsError:
+        print(f'{destination} already exists.')
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+
+if __name__ == '__main__':
+    destination = f'C:/Users/{USER}/AppData/Roaming/.minecraft/saves/Naraka/datapacks/tcg/data'
+
